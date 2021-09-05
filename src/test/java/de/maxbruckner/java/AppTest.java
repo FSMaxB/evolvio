86214a8ab937ac9fc80e857a3e1017ab30b714f6
Max Bruckner <max@maxbruckner.de>
2021-09-05 14:49:43 +0200

    Add assertJ

--- src/test/java/de/maxbruckner/java/AppTest.java
+++ src/test/java/de/maxbruckner/java/AppTest.java
@@ -1,18 +1,19 @@
 package de.maxbruckner.java;
 
 import org.junit.jupiter.api.Test;
+import org.assertj.core.api.WithAssertions;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class AppTest implements WithAssertions {
 	/**
 	 * Rigorous Test :-)
 	 */
 	@Test
 	public void shouldAnswerWithTrue() {
-		assertTrue(true);
+		assertThat(true).isTrue();
 	}
 }
