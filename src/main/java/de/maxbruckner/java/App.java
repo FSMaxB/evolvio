da55236539201392de0e5810b08f0c8e5f45d381
Max Bruckner <max@maxbruckner.de>
2021-09-05 14:07:14 +0200

    Initial commit

---
+++ src/main/java/de/maxbruckner/java/App.java
@@ -0,0 +1,13 @@
+package de.maxbruckner.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
