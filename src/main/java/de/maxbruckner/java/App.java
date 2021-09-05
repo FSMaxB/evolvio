729f35e8a91c8564dfc1fd7ac685497df3ac0992
Max Bruckner <max@maxbruckner.de>
2021-09-05 14:46:10 +0200

    Add .editorconfig

--- src/main/java/de/maxbruckner/java/App.java
+++ src/main/java/de/maxbruckner/java/App.java
@@ -2,12 +2,9 @@ package de.maxbruckner.java;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
--- src/test/java/de/maxbruckner/java/AppTest.java
+++ src/test/java/de/maxbruckner/java/AppTest.java
@@ -1,19 +1,18 @@
 package de.maxbruckner.java;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue(true);
-    }
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
 }
